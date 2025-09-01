d6b0268182f651072744f0464c513e9a4bc71ab3
batikuzey001-debug <batikuzey001@gmail.com>
2025-09-01 19:58:10 +0300

    Update __init__.py

--- app/api/routers/admin_mod/__init__.py
+++ app/api/routers/admin_mod/__init__.py
@@ -2,14 +2,14 @@
 from fastapi import APIRouter
 
 from .sayfalar.loginpage import router as login_router
-from .sayfalar.kod_yonetimi import router as kod_router
-from .sayfalar.turnuvabonus import router as tb_router       # /admin/turnuvabonus
-from .sayfalar.adminyonetim import router as admin_yn_router
 from .sayfalar.panel import router as panel_router
+from .sayfalar.kod_yonetimi import router as kod_router         # /admin/kodyonetimi
+from .sayfalar.turnuvabonus import router as tb_router          # /admin/turnuvabonus
+from .sayfalar.admin_yonetim import router as admin_yn_router   # /admin/users
 
 admin_router = APIRouter()
-admin_router.include_router(login_router)     # /admin/login, /admin/logout
-admin_router.include_router(panel_router)     # /admin (Dashboard)
-admin_router.include_router(kod_router)  # /admin/kod-yonetimi
-admin_router.include_router(tb_router)        # /admin/turnuvabonus
-admin_router.include_router(admin_yn_router)  # /admin/users
+admin_router.include_router(login_router)      # /admin/login, /admin/logout
+admin_router.include_router(panel_router)      # /admin (Dashboard)
+admin_router.include_router(kod_router)        # /admin/kodyonetimi
+admin_router.include_router(tb_router)         # /admin/turnuvabonus
+admin_router.include_router(admin_yn_router)   # /admin/users
