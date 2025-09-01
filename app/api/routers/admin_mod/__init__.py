26eeb08de46031bc7d1c5061938e25d9ffc79fb5
batikuzey001-debug <batikuzey001@gmail.com>
2025-09-01 16:32:04 +0300

    Update __init__.py

--- app/api/routers/admin_mod/__init__.py
+++ app/api/routers/admin_mod/__init__.py
@@ -1,9 +1,11 @@
 # app/api/routers/admin_mod/__init__.py
 from fastapi import APIRouter
 
-from .sayfalar.kod_yonetimi import router as kod_router
-from .sayfalar.turnuva_bonus import router as tb_router
+from .sayfalar.login_sayfasi import router as login_router          # /admin/login, /admin/logout
+from .sayfalar.kod_yonetimi import router as kod_router             # /admin/kod
+from .sayfalar.turnuva_bonus import router as tb_router             # /admin/turnuva-bonus  (varsa)
 
 admin_router = APIRouter()
-admin_router.include_router(kod_router)     # /admin/kod
-admin_router.include_router(tb_router)      # /admin/turnuva-bonus
+admin_router.include_router(login_router)
+admin_router.include_router(kod_router)
+admin_router.include_router(tb_router)
