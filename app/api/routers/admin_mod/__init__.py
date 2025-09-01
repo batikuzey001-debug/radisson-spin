106880d8b80243fa2bd9d10dc38e6d2367b7bf49
batikuzey001-debug <batikuzey001@gmail.com>
2025-09-01 18:56:00 +0300

    Update __init__.py

--- app/api/routers/admin_mod/__init__.py
+++ app/api/routers/admin_mod/__init__.py
@@ -10,6 +10,6 @@ from .sayfalar.panel import router as panel_router
 admin_router = APIRouter()
 admin_router.include_router(login_router)     # /admin/login, /admin/logout
 admin_router.include_router(panel_router)     # /admin (Dashboard)
-admin_router.include_router(kod_router)       # /admin/kod
+admin_router.include_router(kod_router)  # /admin/kod-yonetimi
 admin_router.include_router(tb_router)        # /admin/turnuvabonus
 admin_router.include_router(admin_yn_router)  # /admin/users
