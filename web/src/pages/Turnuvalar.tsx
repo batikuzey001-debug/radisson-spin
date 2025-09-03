98a25b8ebbfe7b7eeda6f3b1a5cc44e2d1077d45
batikuzey001-debug <batikuzey001@gmail.com>
2025-09-03 23:43:31 +0300

    Create Turnuvalar.tsx

---
+++ web/src/pages/Turnuvalar.tsx
@@ -0,0 +1,9 @@
+// web/src/pages/Turnuvalar.tsx
+export default function Turnuvalar() {
+  return (
+    <main style={{color:"#eaf2ff"}}>
+      <h1>Turnuvalar</h1>
+      <p>Turnuva listesi ve detaylar yakında.</p>
+    </main>
+  );
+}
