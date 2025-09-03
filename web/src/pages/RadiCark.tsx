39a1719a02b1ba1874f44764e1d8dcc25f737afd
batikuzey001-debug <batikuzey001@gmail.com>
2025-09-03 23:43:17 +0300

    Create RadiCark.tsx

---
+++ web/src/pages/RadiCark.tsx
@@ -0,0 +1,9 @@
+// web/src/pages/RadiCark.tsx
+export default function RadiCark() {
+  return (
+    <main style={{color:"#eaf2ff"}}>
+      <h1>Radi Çark</h1>
+      <p>Çark sayfası yakında burada.</p>
+    </main>
+  );
+}
