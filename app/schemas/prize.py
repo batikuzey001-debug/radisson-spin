e0290ec558f88cf9c88c9531604ed59f50e04e11
batikuzey001-debug <batikuzey001@gmail.com>
2025-09-05 16:55:40 +0300

    Update prize.py

--- app/schemas/prize.py
+++ app/schemas/prize.py
@@ -1,7 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class PrizeOut(BaseModel):
     id: int
     label: str
-    wheelIndex: int
-    imageUrl: str | None = None
+    wheelIndex: int = Field(..., alias="wheel_index")
+    imageUrl: str | None = Field(None, alias="image_url")
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
