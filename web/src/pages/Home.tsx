f470de0321343c6efc78103282e0704fcbb29b5c
batikuzey001-debug <batikuzey001@gmail.com>
2025-09-05 21:32:48 +0300

    Update Home.tsx

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 // web/src/pages/Home.tsx
-import Hero from "../components/Hero";
 import HeroUpcomingStrip from "../components/HeroUpcomingStrip";
+import Hero from "../components/Hero";
 import QuickBonus from "../components/QuickBonus";
 import EventsGrid from "../components/EventsGrid";
 
